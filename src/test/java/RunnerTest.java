8ab361999eb1f378b7b80e71574d1bdf8401c49f
shreyatulsyan01 <shreyatulsyan01@gmail.com>
2025-01-30 00:41:44 +0530

    Added Runner class

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -1,12 +1,10 @@
-package testRunners;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome=true, tags = "@smoke", features="src/features", glue={"stepDefinitions","common"})
-public class Runner {
+public class RunnerTest {
 
 
 }
